623f444fac78b3a061b58aeb68b6847f9d738640
Evgenia-Untilova-AI233 <evgeniauntilova9@stud.op.edu.ua>
2025-10-25 19:23:16 +0300

    Add workflow for checking SQL and JSON schemas

---
+++ .github/workflows/DataSchema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE financial_data (
+    finance_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    account_balance NUMERIC(10,2),
+    income NUMERIC(10,2),
+    expense NUMERIC(10,2)
+);
+
+CREATE TABLE transactions (
+    transaction_id SERIAL PRIMARY KEY,
+    finance_id INT REFERENCES financial_data(finance_id) ON DELETE CASCADE,
+    amount NUMERIC(10,2) CHECK (amount > 0),
+    type VARCHAR(10) CHECK (type IN ('income', 'expense')),
+    date TIMESTAMP NOT NULL
+);
+
+CREATE TABLE weather_data (
+    weather_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    temperature FLOAT,
+    date TIMESTAMP NOT NULL
+);
+
+ALTER TABLE users
+ADD CONSTRAINT chk_full_name_no_digits
+CHECK (full_name !~ '[0-9]');
+
+ALTER TABLE users
+ADD CONSTRAINT chk_email_format
+CHECK (email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$');
