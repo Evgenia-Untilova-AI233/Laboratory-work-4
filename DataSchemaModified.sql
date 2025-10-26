d52f406937c7f86e4b478056b7a7051016960760
Evgenia-Untilova-AI233 <163032813+Evgenia-Untilova-AI233@users.noreply.github.com>
2025-10-26 13:36:17 +0200

    Update DataSchemaModified.sql

--- DataSchemaModified.sql
+++ DataSchemaModified.sql
@@ -1,28 +1,28 @@
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
-    full_name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL
+    full_name VARCHAR (100) NOT NULL,
+    email VARCHAR (100) UNIQUE NOT NULL
 );
 
 CREATE TABLE financial_data (
     finance_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
-    account_balance NUMERIC(10, 2),
-    income NUMERIC(10, 2),
-    expense NUMERIC(10, 2)
+    user_id INT REFERENCES users (user_id) ON DELETE CASCADE,
+    account_balance NUMERIC (10, 2),
+    income NUMERIC (10, 2),
+    expense NUMERIC (10, 2)
 );
 
 CREATE TABLE transactions (
     transaction_id SERIAL PRIMARY KEY,
-    finance_id INT REFERENCES financial_data(finance_id) ON DELETE CASCADE,
-    amount NUMERIC(10, 2) CHECK (amount > 0),
-    trans_type VARCHAR(10) CHECK (trans_type IN ('income', 'expense')),
+    finance_id INT REFERENCES financial_data (finance_id) ON DELETE CASCADE,
+    amount NUMERIC (10, 2) CHECK (amount > 0),
+    trans_type VARCHAR (10) CHECK (trans_type IN ('income', 'expense')),
     trans_date TIMESTAMP NOT NULL
 );
 
 CREATE TABLE weather_data (
     weather_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    user_id INT REFERENCES users (user_id) ON DELETE CASCADE,
     temperature FLOAT,
     weather_date TIMESTAMP NOT NULL
 );
